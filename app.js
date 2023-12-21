86fd0de4d729caf72e33a31b57e5b9afca2c36c4
Subham Saha <subham@Subhams-MacBook-Pro.local>
2023-12-21 14:25:32 +0530

    final changes

--- app.js
+++ app.js
@@ -55,6 +55,30 @@ const verifyWebhookSignature = (req, res, next) => {
     next();
 };
 
+const storeData = async (data) => {
+    let headersList = {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+    }
+    
+    let bodyContent = JSON.stringify(data);
+    
+    if(data.status=='created'){
+        await fetch("https://aaaedu.in/api/v1/artibot-request/create", { 
+            method: "POST",
+            body: bodyContent,
+            headers: headersList
+        });
+    }else{
+        await fetch("https://aaaedu.in/api/v1/artibot-request/update/"+data.lead_id, { 
+            method: "POST",
+            body: bodyContent,
+            headers: headersList
+        });
+    }
+
+}
+
 app.get('/artibot/test', async function (req, res) {
     console.log(req);
     return res.status(200).json({
@@ -64,12 +88,30 @@ app.get('/artibot/test', async function (req, res) {
 
 app.post('/artibot/webhook', verifyWebhookSignature, async function (req, res) {
     const data = {
-        data: req.body.lead.data,
-        page: req.body.meta.chat_start_page,
-        ip: req.body.meta.ip_address,
-        location: req.body.meta.location,
+        name: req.body.lead.data?.Name?.input,
+        email: req.body.lead.data?.Email?.input,
+        phone: req.body.lead.data['Phone Number']?.input,
+        multiple_choice_query: req.body.lead.data['Multiple Choice Query']?.input,
+        visit_question: req.body.lead.data['Visit Question']?.input,
+        admission_question: req.body.lead.data['Admission Question']?.input,
+        contact_question: req.body.lead.data['Contact Us Question']?.input,
+        course_location_question: req.body.lead.data['Course Location Question']?.input,
+        course_standard_question: req.body.lead.data['Course Standard Question']?.input,
+        school_course_question: req.body.lead.data['Course Standard Question']?.input=='Online' ? req.body.lead.data['Online Course Question']?.input : (req.body.lead.data['Course Standard Question']?.input=='School (Std. VIII to X)' ? req.body.lead.data['School Course Question']?.input : req.body.lead.data['PUC Course Question']?.input),
+        final_callback_question: req.body.lead.data['Finall Call Back Question']?.input!=undefined ? req.body.lead.data['Finall Call Back Question']?.input : (req.body.lead.data['Call Back Question']?.input!=undefined ? req.body.lead.data['Call Back Question']?.input : null),
+        schedule_callback_question: req.body.lead.data['Schedule Call Back Question']?.input,
+        lead_id: req.body.lead.id,
+        page_url: req.body.meta.chat_start_page,
+        ip_address: req.body.meta.ip_address,
+        country: req.body.meta.location.country,
+        latitude: req.body.meta.location.latitude,
+        longitude: req.body.meta.location.longitude,
+        browser: req.body.meta.browser.name,
+        is_mobile: req.body.meta.browser.is_mobile,
+        status: req.body.action,
     }
-    console.log("data: ", data);
+    // console.log("data: ", data);
+    await storeData(data);
     return res.status(200).json({
         message: "working"
     });
